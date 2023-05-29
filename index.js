c148143616a8dcab9a3f30fb68ca3124e6e9c341
wangjianing <jianingw321@outlook.com>
2023-05-29 19:42:13 +0200

    refactor: organize js file by feature

--- index.js
+++ index.js
@@ -1,37 +1,30 @@
-const body = $('body');
 const contactData = [];
 
+// click add button, show form
 const AddBtn = $('.btn-section');
 const popupForm = $('.popup-section');
 AddBtn.on('click', () => popupForm.show());
 
+// close form
 const closeFormBtn = $('.btn-close-form');
-closeFormBtn.on('click', e => popupForm.hide());
+closeFormBtn.on('click', () => popupForm.hide());
 
-// for testing - start
-const searchName = $('input[name="search"]').val();
-console.log(searchName);
-// for testing - end
-
+// form features
 const form = $('.popup-form');
 const submitBtn = $('button[type="submit"]');
 
-const getData = obj => {
-  return obj.val();
-};
-
 const generateContact = index => {
-  const obj = contactData[index];
+  const contact = contactData[index];
 
   const contactHtml = `<div class="contact-unit" id="${index}">
               <div>
                 <div>
-                  <h3 class="contact-name">${obj.firstName}</h3>
-                  <h3 class="contact-name">${obj.lastName}</h3>
+                  <h3 class="contact-name">${contact.firstName}</h3>
+                  <h3 class="contact-name">${contact.lastName}</h3>
                 </div>
                 <div>
-                  <p class="contact-phone">${obj.phone}</p>
-                  <p class="contact-address">${obj.address}</p>
+                  <p class="contact-phone">${contact.phone}</p>
+                  <p class="contact-address">${contact.address}</p>
                 </div>
               </div>
               <button class="btn-delete" id="${index}">Delete</button>
@@ -40,22 +33,41 @@ const generateContact = index => {
   return contactHtml;
 };
 
-const displayContact = obj => {
-  const index = contactData.indexOf(obj);
-
-  // generate
-  const html = generateContact(index);
-
-  // append
-  $('.contact-list').append(html);
-};
-
-// for search, delete
-const displayContacts = arr => {
-  arr.forEach(displayContact);
-};
-
 const contactList = $('.contact-list');
+
+const emptyContactList = () => {
+  contactList.empty();
+};
+
+const displayContact = contact => {
+  const index = contactData.indexOf(contact);
+  const contactHtml = generateContact(index);
+
+  contactList.append(contactHtml);
+};
+
+const displayContacts = (contactArr, scene = '') => {
+  if (!contactArr.length) {
+    let noContact;
+
+    switch (scene) {
+      case 'search':
+        noContact = `<p>You don't have this person's contact.</p>`;
+        break;
+      default:
+        noContact = `<p>You don't have contact yet.</p>`;
+        break;
+    }
+
+    emptyContactList();
+    contactList.append(noContact);
+
+    return;
+  }
+
+  emptyContactList();
+  contactArr.forEach(displayContact);
+};
 
 const firstNameInput = $('input[name="firstname"]');
 const lastNameInput = $('input[name="lastname"]');
@@ -69,7 +81,6 @@ const clearInput = type => {
     lastNameInput.val('');
     phoneInput.val('');
     addressInput.val('');
-    console.log('form');
   } else if (type === 'search') {
     searchInput.val('');
   }
@@ -77,32 +88,21 @@ const clearInput = type => {
 
 const handleSubmit = e => {
   e.preventDefault();
-  const firstName = $('input[name="firstname"]').val();
-  const lastName = $('input[name="lastname"]').val();
-  const phone = $('input[name="phone"]').val();
-  const address = $('input[name="address"]').val();
+
+  const firstName = firstNameInput.val();
+  const lastName = lastNameInput.val();
+  const phone = phoneInput.val();
+  const address = addressInput.val();
 
   //   todo: validate
 
   const formData = { firstName, lastName, phone, address };
 
-  //   body.data([...data, formData]);
-  //   console.log(body.data());
   contactData.push(formData);
-  console.log(contactData);
-  console.log(Object.values(formData));
 
   popupForm.hide();
 
-  //   append contact
-  // $('.contact-list').append(
-  //   generateContact(formData, contactData.indexOf(formData))
-  // );
-
-  $('.contact-list').empty();
-  clearInput('search');
   displayContacts(contactData);
-
   clearInput('form');
 };
 
@@ -110,67 +110,40 @@ form.on('submit', handleSubmit);
 
 // click header to show all contacts
 const header = $('.contact-header');
+
 header.on('click', () => {
-  $('.contact-list').empty();
-  clearInput('search');
   displayContacts(contactData);
+  clearInput('search');
 });
 
-// const deleteBtn = $('.btn-delete');
-
+// delete feature
 const handleDelete = e => {
   const index = e.target.id;
   const removeIndex = `#${index}`;
 
-  //problem, search after delete, still appear
+  // delete from view
   $('.contact-unit').remove(removeIndex);
 
   // delete from data base
   contactData.splice(index, 1);
-  console.log('after deleted data: ', contactData);
 };
 
-// todo, how to repeat
-
+// todo, how to add event listeners to all delete button?
 $(document).on('click', '.btn-delete', handleDelete);
 
+// search feature
 const searchBtn = $('.btn-search');
 
 const handleSearch = () => {
-  const searchInput = $('input[name=search]').val();
-  console.log(typeof searchInput);
+  const searchContent = searchInput.val();
 
-  // arr
   let searchResult = contactData.filter(contact => {
-    console.log(contact);
-    const temp = Object.values(contact);
-    console.log('temp', temp);
-    return temp.includes(searchInput);
+    const contactValues = Object.values(contact);
+    return contactValues.includes(searchContent);
   });
 
-  console.log(searchResult);
-
-  // not found
-  if (!searchResult.length) {
-    // $('.contact-unit').detach();
-    $('.contact-list').empty();
-    const notFound = `<p>You don't have this person's contact yet.</p>`;
-    console.log('not found');
-    $('.contact-list').append(notFound);
-    clearInput('search');
-    // return notFound;
-    return;
-  }
-
-  $('.contact-list').empty();
+  displayContacts(searchResult, 'search');
   clearInput('search');
-  // take in an arr; if there are same names
-  displayContacts(searchResult);
 };
 
 searchBtn.on('click', handleSearch);
-
-// 1.generate single
-// 2.display single
-// - by display, i mean append
-// 3.display list
